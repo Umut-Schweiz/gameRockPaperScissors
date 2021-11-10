9082a38e814e28e14dc05fcb0f6c26efcd767431
Umut2020 <69093672+Umut2020@users.noreply.github.com>
2021-11-10 01:01:19 +0100

    the result page was created

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+const pickUserHand = (hand) => {
+    
+    //hide the current page
+    let hands = document.querySelector(".hands");
+    hands.style.display = "none";
+    //show the next page with the hand yuo picked
+    
+    let contest = document.querySelector(".contest");
+    contest.style.display = "flex"
+
+
+}
